dd73c48e6f19974bafac045d07a14d2bd9fedc18
Rob Napier <rob@neverwood.org>
2023-06-16 17:03:51 -0400

    Initial Commit

---
+++ AppStorageKVOCrash/AppStorageKVOCrashApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStorageKVOCrashApp.swift
+//  AppStorageKVOCrash
+//
+//  Created by Rob Napier on 6/16/23.
+//
+
+import SwiftUI
+
+@main
+struct AppStorageKVOCrashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
